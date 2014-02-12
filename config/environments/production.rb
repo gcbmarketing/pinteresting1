910b13641cb9dc9689edd2d8cf92e6447baf4dce
Greg Berry <greg@gcbmarketing.com>
2014-02-11 21:27:45 -0500

    Implement Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Heroku
+   config.action_mailer.default_url_options = { :host => 'http://warm-headland-9523.herokuapp.com/' }
 end
